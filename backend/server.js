1d73aeff67c96b441c329c7376c6a22dbe347f7d
Sarbottam Abhijeet <abhijeetsarbottam@gmail.com>
2026-01-16 15:06:16 +0530

    Add root route to backend

--- backend/server.js
+++ backend/server.js
@@ -11,6 +11,11 @@ const PORT = process.env.PORT || 5000;
 app.use(cors());
 app.use(express.json());
 
+// Root Route
+app.get('/', (req, res) => {
+    res.send('Lead Management API is running...');
+});
+
 // Routes
 app.use('/api/leads', leadRoutes);
 
